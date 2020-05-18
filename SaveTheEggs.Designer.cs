1d2c8dfa766d64bba5f649c1fc35aa429928a094
ncthanh98 <1654050102thanh@ou.edu.vn>
2020-05-18 09:41:53 +0700

    s

---
+++ SaveTheEggs.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace SaveTheEggs
+{
+    partial class SaveTheEggs
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SaveTheEggs));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.chicken = new System.Windows.Forms.PictureBox();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.Start = new System.Windows.Forms.ToolStripMenuItem();
+            this.Start_De = new System.Windows.Forms.ToolStripMenuItem();
+            this.Start_TrungBinh = new System.Windows.Forms.ToolStripMenuItem();
+            this.Start_Kho = new System.Windows.Forms.ToolStripMenuItem();
+            this.HuongDan = new System.Windows.Forms.ToolStripMenuItem();
+            this.ThongTin = new System.Windows.Forms.ToolStripMenuItem();
+            this.Thoat = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chicken)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(28, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Điểm: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(399, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Mạng: ";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::SaveTheEggs.Properties.Resources.egg;
+            this.pictureBox1.Location = new System.Drawing.Point(48, 270);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(54, 66);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Tag = "Eggs";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::SaveTheEggs.Properties.Resources.egg;
+            this.pictureBox2.Location = new System.Drawing.Point(234, 202);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(54, 66);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Tag = "Eggs";
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::SaveTheEggs.Properties.Resources.egg;
+            this.pictureBox3.Location = new System.Drawing.Point(448, 128);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(54, 66);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 4;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Tag = "Eggs";
+            // 
+            // chicken
+            // 
+            this.chicken.Image = global::SaveTheEggs.Properties.Resources.chicken_normal2;
+            this.chicken.Location = new System.Drawing.Point(223, 607);
+            this.chicken.Name = "chicken";
+            this.chicken.Size = new System.Drawing.Size(93, 83);
+            this.chicken.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.chicken.TabIndex = 5;
+            this.chicken.TabStop = false;
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Enabled = true;
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.gameTimer_Tick);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.Start,
+            this.HuongDan,
+            this.ThongTin,
+            this.Thoat});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(612, 24);
+            this.menuStrip1.TabIndex = 6;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // Start
+            // 
+            this.Start.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.Start_De,
+            this.Start_TrungBinh,
+            this.Start_Kho});
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(59, 20);
+            this.Start.Text = "Bắt đầu";
+            // 
+            // Start_De
+            // 
+            this.Start_De.Name = "Start_De";
+            this.Start_De.Size = new System.Drawing.Size(132, 22);
+            this.Start_De.Text = "Dễ";
+            this.Start_De.Click += new System.EventHandler(this.Start_De_Click);
+            // 
+            // Start_TrungBinh
+            // 
+            this.Start_TrungBinh.Name = "Start_TrungBinh";
+            this.Start_TrungBinh.Size = new System.Drawing.Size(132, 22);
+            this.Start_TrungBinh.Text = "Trung bình";
+            this.Start_TrungBinh.Click += new System.EventHandler(this.Start_TrungBinh_Click);
+            // 
+            // Start_Kho
+            // 
+            this.Start_Kho.Name = "Start_Kho";
+            this.Start_Kho.Size = new System.Drawing.Size(132, 22);
+            this.Start_Kho.Text = "Khó";
+            this.Start_Kho.Click += new System.EventHandler(this.Start_Kho_Click);
+            // 
+            // HuongDan
+            // 
+            this.HuongDan.Name = "HuongDan";
+            this.HuongDan.Size = new System.Drawing.Size(79, 20);
+            this.HuongDan.Text = "Hướng dẫn";
+            this.HuongDan.Click += new System.EventHandler(this.HuongDan_Click);
+            // 
+            // ThongTin
+            // 
+            this.ThongTin.Name = "ThongTin";
+            this.ThongTin.Size = new System.Drawing.Size(74, 20);
+            this.ThongTin.Text = "Thông Tin";
+            this.ThongTin.Click += new System.EventHandler(this.ThongTin_Click);
+            // 
+            // Thoat
+            // 
+            this.Thoat.Name = "Thoat";
+            this.Thoat.Size = new System.Drawing.Size(50, 20);
+            this.Thoat.Text = "Thoát";
+            this.Thoat.Click += new System.EventHandler(this.Thoat_Click);
+            // 
+            // SaveTheEggs
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.ClientSize = new System.Drawing.Size(612, 699);
+            this.Controls.Add(this.chicken);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "SaveTheEggs";
+            this.Text = "Save The Eggs";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.SaveTheEggs_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.SaveTheEggs_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chicken)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox chicken;
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem Start;
+        private System.Windows.Forms.ToolStripMenuItem ThongTin;
+        private System.Windows.Forms.ToolStripMenuItem HuongDan;
+        private System.Windows.Forms.ToolStripMenuItem Start_De;
+        private System.Windows.Forms.ToolStripMenuItem Start_TrungBinh;
+        private System.Windows.Forms.ToolStripMenuItem Start_Kho;
+        private System.Windows.Forms.ToolStripMenuItem Thoat;
+    }
+}
---
+++ SaveTheEggs.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+/*
+ - Họ và tên: Nguyễn Công Thành
+ - MSSV: 1654050102
+ - Môn: LTGD - Lớp: TH71
+*/
+
+namespace SaveTheEggs
+{
+    public partial class SaveTheEggs : Form
+    {
+        bool goleft; //Kiem trai nguoi choi co di chuyen dung qua ben trai khong
+        bool goright; //Kiem trai nguoi choi co di chuyen dung qua ben phai khong
+        int speed = 0; // Toc do tha trung mac dinh
+        int score = 0; //Gia tri diem mac dinh
+        int missed = 0; //Gia tri vo trung mac dinh
+        Random rndY = new Random(); //Tao vi tri ngau nhin Y
+        Random rndX = new Random(); //Tao vi tri ngau nhin X
+        PictureBox splash = new PictureBox();
+
+        public SaveTheEggs()
+        {
+            InitializeComponent();
+           
+        }
+
+        //Thiet lap muc do: De, Trung Binh, Kho
+        private void Start_De_Click(object sender, EventArgs e)
+        {
+            speed = 2;
+        }
+
+        private void Start_TrungBinh_Click(object sender, EventArgs e)
+        {
+            speed = 5; 
+        }
+
+        private void Start_Kho_Click(object sender, EventArgs e)
+        {
+            speed = 8; 
+        }
+
+        //Huong dan
+        private void HuongDan_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(@"
+                                HƯỚNG DẪN 
+                     - Di chuyển bàn phím qua trái hoặc qua
+                       phải để hứng trứng rơi 
+                     - Hứng mỗi quả sẽ được 10 điểm 
+                     - Để rơi 5 quả trứng sẽ thua cuộc", "Hướng Dẫn", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        //Thong Tin
+        private void ThongTin_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Nguyễn Công Thành - MSSV: 1654050102", "Thông Tin", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+        
+        //Thoat
+        private void Thoat_Click(object sender, EventArgs e)
+        {
+            DialogResult h = MessageBox.Show("Bạn có chắc muốn thoát không?", "Thoát", MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
+            if (h == DialogResult.OK)
+                Application.Exit(); 
+        }
+       
+        //Kiem tra nguoi choi nhan phim Trai hay phim Phai
+        private void SaveTheEggs_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                goleft = true;
+            }
+
+            if (e.KeyCode == Keys.Right)
+            {
+                goright = true;
+            }
+
+        }
+
+        private void SaveTheEggs_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                goleft = false;
+            }
+
+            if (e.KeyCode == Keys.Right)
+            {
+                goright = false;
+            }
+        }
+
+        private void gameTimer_Tick(object sender, EventArgs e)
+        {
+            label1.Text = "Điểm: " + score;
+            label2.Text = "Trứng vỡ: " + missed;
+
+            //Neu nhan phim trai "Ga" di chuyen sang ben trai
+            if (goleft == true && chicken.Left > 0)
+            {
+                chicken.Left -= 12;
+                chicken.Image = Properties.Resources.chicken_normal2;
+            }
+
+            //Neu nhan phim phai "Ga" di chuyen sang ben phai va nam trong khung tro choi
+            if (goright == true && chicken.Left + chicken.Width < this.ClientSize.Width)
+            {
+                chicken.Left += 12;
+                chicken.Image = Properties.Resources.chicken_normal;
+            }
+
+            //Kiem tra "Trung" roi
+            foreach (Control X in this.Controls)
+            {
+                if (X is PictureBox && X.Tag == "Eggs")
+                {
+
+                    X.Top += speed;
+
+                    //Neu "Trung" cham san 
+                    if (X.Top + X.Height > this.ClientSize.Height)
+                    {
+                        splash.Image = Properties.Resources.splash;
+                        splash.Location = X.Location;
+                        splash.Height = 59;
+                        splash.Width = 60;
+                        splash.BackColor = System.Drawing.Color.Transparent;
+                        this.Controls.Add(splash);
+                        X.Top = rndY.Next(80, 300) * -1;
+                        X.Left = rndX.Next(5, this.ClientSize.Width - X.Width);
+                        missed++;
+                        chicken.Image = Properties.Resources.chicken_hurt;
+                    }
+
+                    //Neu "Trung" roi dung vao gio cua "Ga"
+                    if (X.Bounds.IntersectsWith(chicken.Bounds))
+                    {
+                        X.Top = rndY.Next(100, 300) * -1;
+                        X.Left = rndX.Next(5, this.ClientSize.Width - X.Width);
+                        score+= 10;
+                    }
+
+                    //Neu "Trung" roi hon 5 qua
+                    if (missed > 5)
+                    {
+                        gameTimer.Stop();
+                        DialogResult h = MessageBox.Show("Game Over!" + "\r\n" + "Nhấn OK để chơi lại", "Game Over", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+                            reset();
+                       
+                    }
+
+                }
+            }
+        }
+
+        //Reset lai tro choi
+        private void reset()
+        {
+            foreach (Control X in this.Controls)
+            {
+                if (X is PictureBox && X.Tag == "Eggs")
+                {
+                    X.Top = rndY.Next(80, 300) * -1;
+                    X.Left = rndX.Next(5, this.ClientSize.Width - X.Width);
+                }
+            }
+            chicken.Left = this.ClientSize.Width / 2;
+            chicken.Image = Properties.Resources.chicken_normal2;
+            score = 0;
+            missed = 0;
+            goleft = false;
+            goright = false;
+            gameTimer.Start();
+        }
+       
+    }
+}
